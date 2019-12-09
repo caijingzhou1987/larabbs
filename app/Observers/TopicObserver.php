9bd3b44fbac971852362d487d6a144a3773e3876
cjz <3193619731@qq.com>
2019-12-09 06:27:05 +0000

    翻译URL标示

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,6 +3,8 @@
 namespace App\Observers;
 
 use App\Models\Topic;
+use Stichoza\GoogleTranslate\GoogleTranslate;
+use Overtrue\Pinyin\Pinyin;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -23,5 +25,16 @@ class TopicObserver
     {
         $topic->body = clean($topic->body,'user_topic_body');
     	$topic->excerpt = make_excerpt($topic->body);
+        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
+        if ( ! $topic->slug) {
+            try{
+                $tr = new GoogleTranslate();
+                $tr->setUrl('http://translate.google.cn/translate_a/single');
+                $topic->slug = $tr->translate($topic->title);
+            }catch(ErrorException $e){
+                $pinyin = new Pinyin();
+                $topic->slug = $pinyin->permalink($topic->title);
+            }
+        }
     }
 }
